04e544cebfedd76b7fabf19cb670f3ceeae02c6f
Joe Edgerton <jedgerton@umces.edu>
2022-05-11 10:46:36 -0400

    changing the parameter names to more closley align with IAN's project structure

--- R/project_file_setup.R
+++ R/project_file_setup.R
@@ -1,10 +1,10 @@
 # `standard` `parameters` is obviously dummy for whatever conventions you use
 # name is the name of the actual project
 
-setup_basic_project <- function(parameters, name, base_dir = "."){
+setup_basic_project <- function(IAN_project, indicator, base_dir = "."){
   usethis::ui_done("setting up standard folder structure")
   root <- fs::path(base_dir, glue::glue(format(Sys.Date(),
-                                               "%Y-%m-%d"),"_{parameters}_{name}"))
+                                               "%Y-%m-%d"),"_{IAN_project}_{indicator}"))
   usethis::create_project(path = root, open = FALSE)
   # add the standard internal structure
   sub <- c("data","output")
@@ -19,7 +19,7 @@ setup_basic_project <- function(parameters, name, base_dir = "."){
         template = "skeleton.Rmd",
         save_as = fs::path("R", glue::glue(format(Sys.Date(),
                                                   "%Y-%m-%d"),"_{parameters}_{name}.Rmd")),
-        data = list(parameters = parameters),
+        data = list(IAN_project = IAN_project),
         # use the actual package name
         package = "IANindicatoRs",
         open = TRUE
